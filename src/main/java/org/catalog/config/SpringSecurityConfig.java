75042945cd4c4b7938d9bdb6b52ac94566682451
Moorka <olgapegaska@gmail.com>
2017-06-11 18:39:37 +0300

    work

--- src/main/java/org/catalog/config/SpringSecurityConfig.java
+++ src/main/java/org/catalog/config/SpringSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
